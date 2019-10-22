4213948a25a5b874fb86fe6a3a39523675368aa5
ramonrrezende <ramonrocharezende@gmail.com>
2019-10-22 15:41:04 -0300

    Criação de funções para percorrer e criar o grafo

--- main.rkt
+++ main.rkt
@@ -1,23 +1,48 @@
 #lang racket
 
+;define estrutura para um nó
 (struct node(id marked childrens))
 
-(define n1 (#f (list 2 3)))
+(define n1 (node 1 #f (list 4 2 3)))
 
-(define n2 (#f (list 3)))
+(define n2 (node 2 #f (list 3)))
 
-(define n3 (#f (list 1)))
+(define n3 (node 3 #f (list 1)))
+
+(define n4 (node 4 #f (list 4)))
 
 (define g (make-hash))
 
-(define (create_graph g nodes)
-    (hash-set! g (first nodes)
-    (create_graph g (rest nodes))
-    ))
+(define nodes (list n1 n2 n3 n4))
 
-(define (create_graph g nodes)
-    (hash-set! g
-        ((node-id (first nodes)) 
-        (node-childrens (first nodes))) )
-    (if (> (length (rest nodes)) 0) (create_graph g (rest nodes))) (display "Done!\n") )
 
+;Cria um grafo a partir de uma hash table vazia e uma lista de nós
+(define (create_graph g nodes)
+    (hash-set! g (node-id (first nodes)) (first nodes) );associa o nó a uma chave da hash table(a chave é o id do nó)
+    (if (> (length (rest nodes)) 0) (create_graph g (rest nodes)) (display "Done!\n"));chama a função recursivamente para adicionar todos os nós da lista
+)
+
+(define (explore graph ch)
+   (when (not (node-marked (hash-ref graph (first ch))))
+        (hash-set! graph (first ch) (struct-copy node (hash-ref graph (first ch)) [marked #t]));marca o nó como visitado e atualiza a hash table
+        (display (first ch));adicionar operação relevante aqui
+        (display "\n")
+        (when (> (length (rest ch)) 0) (explore graph (rest ch)))
+    )
+)
+
+;Função que percorre o grafo, partindo de um nó inicial, marcando os nós visitados
+(define (run graph bgn)
+    (hash-set! graph bgn (struct-copy node (hash-ref graph bgn) [marked #t]));cria uma cópia da estrutura com o atributo "marcado" como verdade e associa à chave da hash table
+    (display  bgn);Mostra o id do nó visitado(substituir por operação relevante posteriormente)
+    (display "\n")
+    (if (> (length (node-childrens (hash-ref graph bgn))) 0)
+        (explore graph (node-childrens (hash-ref graph bgn)))
+        (display "END!\n")
+    )
+)
+
+(create_graph g nodes)
+
+(display "\n")
+(run g 1)
