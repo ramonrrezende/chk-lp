b20b9e680dae558671922595cf2b2f8b1c237fbb
ramonrrezende <ramonrocharezende@gmail.com>
2019-10-25 00:19:16 -0300

    Creating files

--- main.rkt
+++ main.rkt
@@ -1,7 +1,5 @@
 #lang racket
-
-;define estrutura para um nó
-(struct node(id marked childrens))
+(require "node.rkt" "graph.rkt")
 
 (define n1 (node 1 #f (list 4 2 3)))
 
@@ -14,33 +12,6 @@
 (define g (make-hash))
 
 (define nodes (list n1 n2 n3 n4))
-
-
-;Cria um grafo a partir de uma hash table vazia e uma lista de nós
-(define (create-graph g nodes)
-    (hash-set! g (node-id (first nodes)) (first nodes) );associa o nó a uma chave da hash table(a chave é o id do nó)
-    (if (> (length (rest nodes)) 0) (create-graph g (rest nodes)) (display "Done!\n"));chama a função recursivamente para adicionar todos os nós da lista
-)
-
-(define (explore graph ch)
-   (when (not (node-marked (hash-ref graph (first ch))))
-        (hash-set! graph (first ch) (struct-copy node (hash-ref graph (first ch)) [marked #t]));marca o nó como visitado e atualiza a hash table
-        (display (first ch));adicionar operação relevante aqui
-        (newline)
-        (when (> (length (rest ch)) 0) (explore graph (rest ch)))
-    )
-)
-
-;Função que percorre o grafo, partindo de um nó inicial, marcando os nós visitados
-(define (run graph bgn)
-    (hash-set! graph bgn (struct-copy node (hash-ref graph bgn) [marked #t]));cria uma cópia da estrutura com o atributo "marcado" como verdade e associa à chave da hash table
-    (display  bgn);Mostra o id do nó visitado(substituir por operação relevante posteriormente)
-    (newline)
-    (if (> (length (node-childrens (hash-ref graph bgn))) 0)
-        (explore graph (node-childrens (hash-ref graph bgn)))
-        (display "END!\n")
-    )
-)
 
 (create-graph g nodes)
 
